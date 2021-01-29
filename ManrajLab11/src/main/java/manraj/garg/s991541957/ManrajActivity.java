eb9fa1973648e9e7e362289e4d582699900326b2
Manraj <manraj_g93@hotmail.com>
2021-01-29 02:07:06 -0500

    Initial commit - setup of classes, modules, string.xml, values-fr based on assignment requirements

---
+++ ManrajLab11/src/main/java/manraj/garg/s991541957/ManrajActivity.java
@@ -0,0 +1,18 @@
+/*
+Manraj Garg s991541957
+This is assignment 1 to demonstrate the use of activities and how to invoke 3rd party app
+ */
+package manraj.garg.s991541957;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManrajActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
