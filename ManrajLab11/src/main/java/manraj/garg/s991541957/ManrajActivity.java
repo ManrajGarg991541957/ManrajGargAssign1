a5c795f181a0d4c50615b509c2def551c5764c96
Manraj <manraj_g93@hotmail.com>
2021-01-31 18:11:54 -0500

    Add textview, edittext, and 2 buttons to main.xml, changed background color to light blue, layout to linear with vertical orientation, created methods for callIntent and gave functionality to the 2 buttons

--- ManrajLab11/src/main/java/manraj/garg/s991541957/ManrajActivity.java
+++ ManrajLab11/src/main/java/manraj/garg/s991541957/ManrajActivity.java
@@ -6,7 +6,14 @@ package manraj.garg.s991541957;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class ManrajActivity extends AppCompatActivity {
 
@@ -14,5 +21,33 @@ public class ManrajActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void callIntent(View view){
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.btnBrowser:
+
+                Context context = getApplicationContext();
+                CharSequence text = getString(R.string.toast_label);
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.google.ca"));
+                startActivity(intent);
+                break;
+            case R.id.btnActivity:
+
+                Snackbar.make(view, "The First SnackBar Button was clicked.", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+                intent = new Intent(this, GargActivity.class);
+                startActivity(intent);
+            default:
+                break;
+        }
     }
 }
